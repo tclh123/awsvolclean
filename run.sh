40109e3f93520299b1f8567dd285167286dbbbb2
Harry Lee <harry.lee@smarkets.com>
2020-12-01 17:28:58 +0000

    update run.sh script

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 . venv/bin/activate
-./volclean.py --region eu-west-1 -t Name:^kubernetes-dynamic-pvc --age 7
+./volclean.py --region eu-west-1 -t Name:^kubernetes-dynamic-pvc --age 7 -y
+
+./volclean.py --region eu-west-1 -t OS_Version:^Ubuntu -R ami --age 365 -v -y
+./volclean.py --region eu-west-1 -t OS_Version:^Ubuntu -R snapshots --age 365 -v -y
